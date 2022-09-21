2726f6fec7e52f395dba802712786ca19006c3b0
GermanM23 <german230900@icloud.com>
2022-09-21 21:09:06 +0300

    add fixes hw6

--- script.js
+++ script.js
@@ -1,18 +1,28 @@
 const minSearch = (firstNum, secondNum, thirdNum, fourthNum) => {
+  if (typeof (firstNum && secondNum && thirdNum && fourthNum) === "string") {
+    firstNum = Number(firstNum);
+    secondNum = Number(secondNum);
+    thirdNum = Number(thirdNum);
+    fourthNum = Number(fourthNum);
+  }
   if (typeof (firstNum && secondNum && thirdNum && fourthNum) === "number") {
     // console.log(Math.min(firstNum, secondNum, thirdNum, fourthNum));
     return Math.min(firstNum, secondNum, thirdNum, fourthNum);
   } else {
     // console.log("Wrong number of arguments");
-    return "false";
+    return "Something went, wrong. We cannot find the minimum number";
   }
 };
-minSearch(6, 8, 11, 0);
+minSearch("6", 8, null, "-1");
 
-const gcd = (x, y) => {
+const greatestCommonDivider = (x, y) => {
+  if (typeof (x && y) === "string") {
+    x = Number(x);
+    y = Number(y);
+  }
   if (typeof x !== "number" || typeof y !== "number") {
     // console.log("false");
-    return false;
+    return "Something went, wrong.";
   } else {
     x = Math.abs(x);
     y = Math.abs(y);
@@ -26,9 +36,12 @@ const gcd = (x, y) => {
   }
 };
 
-gcd(8, 22);
+greatestCommonDivider("8", "22");
 
 const isPerfect = (num1) => {
+  if (typeof num1 === "string") {
+    num1 = Number(num1);
+  }
   if (typeof num1 === "number") {
     let temp = 0;
     for (let i = 1; i <= num1 / 2; i++) {
@@ -48,9 +61,13 @@ const isPerfect = (num1) => {
     return "Error occured, please try again";
   }
 };
-isPerfect(21);
+isPerfect("28");
 
 const sumTo = (number1, number2) => {
+  if (typeof (number1 && number2) === "string") {
+    number1 = Number(number1);
+    number2 = Number(number2);
+  }
   if (typeof (number1 && number2) === "number") {
     let sum = 0;
     for (let i = number1; i <= number2; i++) {
@@ -63,9 +80,12 @@ const sumTo = (number1, number2) => {
     return "Error occured, please try again";
   }
 };
-sumTo(null, 11);
+sumTo("1", "3");
 
 const convetCtoF = (celsius) => {
+  if (typeof celsius === "string") {
+    celsius = Number(celsius);
+  }
   if (typeof celsius === "number") {
     // console.log(celsius * (9 / 5) + 32);
     return celsius * (9 / 5) + 32;
@@ -73,9 +93,11 @@ const convetCtoF = (celsius) => {
     return "Error occured, please try again";
   }
 };
-convetCtoF(22);
+convetCtoF("22");
 
 const getRandomNum = (min, max) => {
+  min = 0;
+  max = 40;
   if (typeof (min && max) === "number") {
     min = Math.ceil(min);
     max = Math.floor(max);
@@ -86,4 +108,5 @@ const getRandomNum = (min, max) => {
     return "Error occured, please try again";
   }
 };
-getRandomNum(0, 40);
+
+getRandomNum();
